7e74c11845e6590a0abf72f1d347f25e64e39db3
Mathias Buus <mathiasbuus@gmail.com>
2016-10-08 14:21:01 -0700

    bump tweetnacl and always test browser build

--- test.js
+++ test.js
@@ -1,29 +1,36 @@
 var tape = require('tape')
-var encryption = require('./')
 
-tape('generates keys and nonce', function (t) {
-  t.same(encryption.key().length, 32, '32 byte keys')
-  t.same(encryption.nonce().length, 24, '24 byte nonces')
-  t.end()
-})
+run(require('./'), false)
+run(require('./browser'), true)
 
-tape('encrypts and decrypts', function (t) {
-  var key = encryption.key()
-  var nonce = encryption.nonce()
+function run (encryption, browser) {
+  var prefix = browser ? '[browser-build] ' : ''
 
-  var box = encryption.encrypt(Buffer('hi'), nonce, key)
+  tape(prefix + 'generates keys and nonce', function (t) {
+    t.same(encryption.key().length, 32, '32 byte keys')
+    t.same(encryption.nonce().length, 24, '24 byte nonces')
+    t.end()
+  })
 
-  t.notSame(box, Buffer('hi'), 'message was encrypted')
-  t.same(encryption.decrypt(box, nonce, key), Buffer('hi'), 'same message out')
-  t.end()
-})
+  tape(prefix + 'encrypts and decrypts', function (t) {
+    var key = encryption.key()
+    var nonce = encryption.nonce()
 
-tape('scalar multiplication', function (t) {
-  var keys = encryption.scalarMultiplicationKeyPair()
-  var otherKeys = encryption.scalarMultiplicationKeyPair()
-  var sharedKey = encryption.scalarMultiplication(keys.secretKey, otherKeys.publicKey)
-  var otherSharedKey = encryption.scalarMultiplication(otherKeys.secretKey, keys.publicKey)
+    var box = encryption.encrypt(Buffer('hi'), nonce, key)
 
-  t.same(sharedKey, otherSharedKey, 'shared key should be the same')
-  t.end()
-})
+    t.notSame(box, Buffer('hi'), 'message was encrypted')
+    t.same(encryption.decrypt(box, nonce, key), Buffer('hi'), 'same message out')
+    t.end()
+  })
+
+  tape(prefix + 'scalar multiplication', function (t) {
+    var keys = encryption.scalarMultiplicationKeyPair()
+    var otherKeys = encryption.scalarMultiplicationKeyPair()
+    var sharedKey = encryption.scalarMultiplication(keys.secretKey, otherKeys.publicKey)
+    var otherSharedKey = encryption.scalarMultiplication(otherKeys.secretKey, keys.publicKey)
+
+    t.same(sharedKey, otherSharedKey, 'shared key should be the same')
+    t.end()
+  })
+}
+
